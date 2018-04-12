6bd5a48430938fe306056c85e7b91804503a7e84
zyfdegh <zyfdegg@gmail.com>
2018-04-12 02:40:56 -0400

    remove internal net config in node

--- start-mysterium-server.sh
+++ start-mysterium-server.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo docker run --cap-add NET_ADMIN --net mysterium --net host --publish "1194:1194" --name mysterium-node -d mysterium/server:latest
+sudo docker run --cap-add NET_ADMIN --net host --publish "1194:1194" --name mysterium-node -d mysterium/server:latest
